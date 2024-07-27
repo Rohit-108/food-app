5c518d33e6bd6a6f194448f808b4d72edb6fef27
Rohit-108 <rohitsharma1082001@gmail.com>
2024-07-27 19:41:34 +0530

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import About from "./components/About";
+// import About from "./components/About";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Error from "./components/Error";
-import Contact from "./components/Contact.jsx";
+// import Contact from "./components/Contact.jsx";
 import RestaurantMenu from "./components/RestaurantMenu.jsx";
 import Profile from "./components/Profile.jsx";
 import {Provider} from "react-redux";
 import store from "./utils/store.jsx";
 import Cart from "./components/Cart.jsx"
+import {lazy, Suspense} from "react";
+import Login from "./components/Login.jsx";
 
+
+// Chunking
+// Code Splitting 
+// Dynamic Bundling
+// Lazy Loading
+
+const About = lazy(() => import("./components/About.jsx"));
+const Contact = lazy(() => import("./components/Contact.jsx"));
 // import Login from "./components/Login.jsx";
  const App = () => {
   return (
@@ -45,7 +55,11 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "about",
-        element: <About />,
+        element:(
+          <Suspense fallback={<hi>Loading...</hi>}>
+            <About />
+          </Suspense>
+        ) ,
         children: [
           {
             // nested routing
@@ -56,7 +70,11 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/contact",
-        element: <Contact />,
+        element:(
+        <Suspense fallback={<hi>Loading...</hi>}>
+        <Contact />
+      </Suspense>
+    ) ,
       },
       {
         path:"/restaurant/:resId",
@@ -65,6 +83,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/cart",
         element: <Cart />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       }
       
     ],
