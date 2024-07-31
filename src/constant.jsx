c007c35222f8398370e59d4609bc762f2be7feed
Rohit-108 <rohitsharma1082001@gmail.com>
2024-07-31 11:39:33 +0530

    api issue fix

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,16 +1,16 @@
 // src/constants.js
 
-// Base URL for Swiggy image CDN
-export const IMG_CDN_URL = import.meta.env.VITE_IMG_CDN_URL;
+// Image CDN URL for Restaurant card
+export const IMG_CDN_URL = `${import.meta.env.VITE_REACT_MEDIA_ASSETS_BASE_URL}image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/`;
 
 // Image CDN URL for Restaurant Menu
-export const ITEM_IMG_CDN_URL = import.meta.env.VITE_ITEM_IMG_CDN_URL;
+export const ITEM_IMG_CDN_URL = `${import.meta.env.VITE_REACT_MEDIA_ASSETS_BASE_URL}image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/`;
 
 // Swiggy API to get Restaurant data using foodfire server
-export const FOODVILLA_API_URL = import.meta.env.VITE_FOODVILLA_API_URL;
+export const FOODVILLA_API_URL = `${import.meta.env.VITE_REACT_FOODVILLA_APP_BASE_URL}restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING`;
 
 // Swiggy API to get Restaurant Menu data using foodfire server
-export const FOODVILLA_MENU_API_URL = import.meta.env.VITE_FOODVILLA_MENU_API_URL;
+export const FOODVILLA_MENU_API_URL = `${import.meta.env.VITE_REACT_FOODVILLA_APP_BASE_URL}menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=`;
 
 export const restrauntList= 
 [
